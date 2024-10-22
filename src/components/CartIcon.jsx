7494a107f271065f28586cb8f75ef0142267a031
Hamed Mahjoobi <hmdhamed88@gmail.com>
2024-10-22 23:24:26 +0330

    update project

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,18 +1,17 @@
 // src/components/CartIcon.js
-import React from 'react';
-import { FiShoppingCart } from 'react-icons/fi';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CartIcon = () => {
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   return (
-    <div >
+    <div>
       <Link to="/cart">
         <div className="relative">
- 
-          <FiShoppingCart className='text-3xl'/>
+          <FiShoppingCart className="text-3xl" />
           <span className="absolute -top-3 -right-2 bg-amber-700 text-white rounded-full px-2 py-1 text-xs">
             {totalQuantity}
           </span>
